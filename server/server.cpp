0dd0b7d770c2a7b70d8861a97e3e6e81c78eebcb
Ricardo Oda <oda.ric@gmail.com>
2012-08-06 21:12:51 -0300

    comeco do desenvolvimento do servidor

--- server/server.cpp
+++ server/server.cpp
@@ -8,8 +8,95 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include "server.hpp"
 
-int main(void) {
-	puts("Hello World!!!");
-	return EXIT_SUCCESS;
-}
+using namespace WebSocket;
+class WebSocketServer
+{
+private:
+	int intPorta;
+	bool boolExecutando;
+	//TcpListener tcplWebSocketServer;
+	//Thread threadEsperaSocketCliente;
+
+	void error(const char *msg)
+	{
+		perror(msg);
+		exit(1);
+	}
+
+
+public:
+	WebSocketServer()
+	{
+		intPorta = 0;
+		boolExecutando = false;
+	}
+
+	bool isBoolExecutando() const {
+		return boolExecutando;
+	}
+
+	int getIntPorta() const {
+		return intPorta;
+	}
+
+	void setIntPorta(int intPorta) {
+		if (!boolExecutando)
+		{
+			this->intPorta = intPorta;
+		}
+
+	}
+
+	void Start()
+	{
+		if (!boolExecutando)
+		{
+			//tcplWebSocketServer = new TcpListener(IPAddress.Any, intPorta);
+			//threadEsperaSocketCliente = new Thread(EsperarSocketCliente);
+			this.OnBeforeStart();
+			//tcplWebSocketServer.Start();
+			this.OnAfterStart();
+			boolExecutando = true;
+			//threadEsperaSocketCliente.Start();
+		}
+	}
+/*
+	void Stop()
+	{
+		if (boolExecutando)
+		{
+			boolExecutando = false;
+			threadEsperaSocketCliente.Abort();
+			this.OnBeforeStop();
+			tcplWebSocketServer.Stop();
+			this.OnAfterStop();
+			threadEsperaSocketCliente = null;
+			tcplWebSocketServer = null;
+		}
+	}
+
+private:
+	void EsperarSocketCliente()
+	{
+		while (boolExecutando)
+		{
+			new Thread(OnAcceptSocketClienteInterno).Start(tcplWebSocketServer.AcceptSocket());
+		}
+	}
+
+	void OnAcceptSocketClienteInterno(object o)
+	{
+		OnAcceptSocketCliente((Socket)o);
+	}
+*/
+public:
+	//virtual void OnAcceptSocketCliente(Socket sockCliente);
+	virtual void OnBeforeStart();
+	virtual void OnAfterStart();
+	virtual void OnBeforeStop();
+	virtual void OnAfterStop();
+	virtual ~WebSocketServer(); //não sei pq precisa..
+};
