5fa9f1d168e197b9a4b0bc09430d823a7491bfe7
kodo <mkaosm@gmail.com>
2012-08-15 17:09:50 -0300

    er

---
+++ old/server/SimpleWebSocketServer.hpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <vector>
+#include <string>
+
+#ifndef SWSS_HPP_
+#define SWSS_HPP_
+
+using namespace std;
+
+const unsigned int RCVBUFSIZE = 32;
+
+
+namespace SimpleWebSocketServer
+{
+  class SimpleWebSocketServer
+  {
+  private:
+    inline const unsigned int rol(const unsigned int, const unsigned int);
+    void innerHash(unsigned int, unsigned int);
+  public:
+
+#endif /* SWSS_HPP_ */
